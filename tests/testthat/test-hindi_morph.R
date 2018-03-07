8c99854b962fbbe6cd8fbb09934834c83d9600f9
Yuya Matsumura <y.matsumura@infocom.co.jp>
2018-03-07 12:05:07 +0900

    cahnge column name "morph" to "pos"

--- tests/testthat/test-hindi_morph.R
+++ tests/testthat/test-hindi_morph.R
@@ -7,6 +7,6 @@ test_that("Moprph", {
   dat <- HindiMorph:::tw_text2
   res <- hindi_morph(dat, text)
   expected <- "\\u0924\\u0941\\u092e"
-  expect_equal(stringi:: stri_escape_unicode(as.character(res[5, 1])),
+  expect_equal(stringi:: stri_escape_unicode(as.character(res[5, 2])),
                expected)
 })
